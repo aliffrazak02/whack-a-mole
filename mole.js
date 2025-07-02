c32fc80a85ef587be2461795a19192e01c5b9444
Aliff Iman <aliffmlg@gmail.com>
2025-07-02 11:20:18 +0800

    Add mole image display and adjust tile image size in Whack-a-Mole

--- mole.js
+++ mole.js
@@ -1,3 +1,6 @@
+let currMoleTile;
+
+
 window.onload = function() {
     setGame();
 };
@@ -9,4 +12,24 @@ function setGame() {
         title.id = i.toString();
         document.getElementById("board").appendChild(title);
     }
+
+    setInterval(setMole, 2000); // Set a mole every second
+}
+
+function getRandomTile() {
+    let num = Math.floor(Math.random() * 9); // Random number between 0 and 8
+    while (num === currMoleTile) { // Ensure the mole doesn't appear in the same tile
+        num = Math.floor(Math.random() * 9);
+    }
+    currMoleTile = num;
+    return num.toString();
+}
+
+function setMole() {
+    let mole = document.createElement("img");
+    mole.src = "assets/monty-mole.png"; 
+
+    let num = getRandomTile();
+    currMoleTile = document.getElementById(num);
+    currMoleTile.appendChild(mole);
 }
